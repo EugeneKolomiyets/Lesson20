488b6b35da119c6df98491dfe6e698894d3ea3b2
EugeneKolomiyets <evgenum@gmail.com>
2021-05-05 02:44:43 +0300

    work at home2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,14 @@
 package com.company;
 
+
+import java.io.IOException;
+import java.io.UncheckedIOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Main {
@@ -29,7 +34,7 @@ public class Main {
             System.out.println("if you want to search directories(type 'dir') otherwise search files");
             Boolean dirSearch = false;
             String dirString = scan.nextLine();
-            if (dirString.equals("yes")) {
+            if (dirString.equals("dir")) {
                 dirSearch=true;
             }
 
@@ -53,7 +58,7 @@ public class Main {
 
             Boolean filterSize = false;
             int minSize=0, maxSize=0;
-            System.out.println("filter size filter (type 'yes' otherwise no filter)");
+            System.out.println("filter size filter (type 'yes') otherwise no filter)");
             String sizeString=scan.nextLine();
             if (sizeString.equals("yes")){
                 filterSize=true;
@@ -61,9 +66,19 @@ public class Main {
                 minSize = scan.nextInt();
                 System.out.println("input maximum size in Kb");
                 maxSize = scan.nextInt();
+                scan.nextLine();
             }
 
-            //path..d;
+            Boolean textFileSearch = false;
+            System.out.println("filter text files by keywords (type 'yes') otherwise no filter)");
+            String keyWord="";
+            String textString=scan.nextLine();
+            if (textString.equals("yes")){
+                textFileSearch=true;
+                System.out.println("input substring");
+                keyWord = scan.nextLine();
+            }
+
             if (Files.exists(path)) {
                 try (Stream<Path> walk = Files.walk(path)) {
                     String finalRegularString = regularString;
@@ -73,13 +88,17 @@ public class Main {
                     Boolean finalFilterReg = filterReg;
                     Boolean finalFilterSize = filterSize;
                     int finalMinSize = minSize;
+                    int finalMaxSize = maxSize;
+                    Boolean finalTextFileSearch = textFileSearch;
+                    String finalKeyWord = keyWord;
                     walk
                            //.filter(Files::isRegularFile)
                             //.filter()
                            .filter(path1 -> (finalFilterReg ? path1.toFile().getName().matches(finalRegularString):true) /* "d.*" */
-                           && (finalFilterName ?path1.toString().contains(finalSubstring):true)
-                           && (finalDirSearch ?path1.toFile().isDirectory():path1.toFile().isFile())
-                           && (finalFilterSize ?true:getSizeKb(path1)>= finalMinSize))
+                           && (finalFilterName ? path1.toString().contains(finalSubstring):true)
+                           && (finalDirSearch ? path1.toFile().isDirectory():path1.toFile().isFile())
+                           && (finalFilterSize ? (getSizeKb(path1)>= finalMinSize && getSizeKb(path1)<= finalMaxSize):true)
+                           && (finalTextFileSearch ? testFile(path1, finalKeyWord):true))
                             .map(Path::toAbsolutePath)
                             .forEach(System.out::println);
                 } catch (java.io.IOException e) {
@@ -90,5 +109,18 @@ public class Main {
     }
     static private int getSizeKb(Path path1){
         return (int)(path1.toFile().length()/1024);
+    }
+
+    static private boolean testFile(Path path1,String keyWord){
+        //System.out.println(path1);
+        try (Stream<String> stream = Files.lines(path1)) {
+            return stream.map(String::toLowerCase)
+                    .anyMatch(line1 -> line1.contains(keyWord));
+        }
+        catch (UncheckedIOException | IOException e) {
+            // Handle a potential exception
+        }
+
+        return false;
     }
 }
